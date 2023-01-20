b53af7c2b1a469ccafbe397cf18e720ca95250d5
LorenzoNicco <lorenzoniccolai91@gmail.com>
2023-01-20 16:12:46 +0100

    Add test methods

--- js/script.js
+++ js/script.js
@@ -6,7 +6,18 @@ createApp({
     data () {
         return {
             message: "Hello Vue!",
-            pic: "takashi-miyazaki-wrHRP10ZW70-unsplash.jpg"
+            pic: "takashi-miyazaki-wrHRP10ZW70-unsplash.jpg",
+            centerClass: "center",
+            bgClass: "",
+            pictureClass: ""
+        }
+    },
+    methods: {
+        imageStyle: function() {
+            return this.pictureClass =  "shadow" + " " + "center";
+        },
+        bgStyle: function() {
+            return this.bgClass = "flex" + " " + "bg-color" + " " + "bg-dimension";
         }
     }
 }).mount('#app');
